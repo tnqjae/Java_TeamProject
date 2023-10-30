89746bb903d7d785fa4f450a09361ece4664430d
SteffenLee <tnqjae@naver.com>
2023-10-30 14:28:00 +0900

    Changes

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,34 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
+
 public class MainFrame extends JFrame {
+    ImageIcon bg;
+    ImageIcon titleImage;
 
-    public JLabel backGround(){
-        ImageIcon bg = new ImageIcon("./img/StartBackground.jpeg");
-        JLabel background = new JLabel(bg);
-        background.setSize(1200,750);
-        background.setLocation(0,0);
+    public MainFrame() {
+        super("Hogwarts Escape");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
 
-        return background;
+        // 이미지 로딩 스레드 시작
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                loadImages(); // 이미지 로드
+
+                // 이미지 로드 후 UI 업데이트
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        addBackground(); // 배경 추가
+                        addTitle(); // 타이틀 추가
+                        setSize(1200, 750);
+                        setVisible(true);
+                    }
+                });
+            }
+        });
+        thread.start();
     }
 
-    public JLabel TitleBar() {
-        ImageIcon titleImage = new ImageIcon("./img/title.png");
+    private void loadImages() {
+        // 이미지 로딩을 수행하는 부분
+        // 이전 코드와 동일하게 이미지 로드하는 부분 추가
+        bg = new ImageIcon("./img/StartBackground.jpeg");
+        titleImage = new ImageIcon("./img/title.png");
+    }
+
+    private void addBackground() {
+        JLabel background = new JLabel(bg);
+        background.setSize(bg.getIconWidth(), bg.getIconHeight());
+        background.setLocation(0, 0);
+        add(background);
+    }
+
+    private void addTitle() {
         JLabel title = new JLabel(titleImage);
         title.setSize(titleImage.getIconWidth(), titleImage.getIconHeight());
         title.setLocation(420, 80);
-        return title;
+        add(title);
     }
 
-    public MainFrame(){
-        super("Hoguwarts Escape");
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane();
-        setLayout(null);
-
-        add(TitleBar());
-        add(backGround());
-        setSize(1200,750);
-        setVisible(true);
-    }
 }
