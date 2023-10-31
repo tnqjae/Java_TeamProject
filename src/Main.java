46e6924c6aca97c226b9ae6b9b002abb2e41f2a6
SteffenLee <tnqjae@naver.com>
2023-10-31 15:34:00 +0900

    Add new Frame to Standardization of Frames

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main{
+import javax.swing.*;
+public class Main extends JFrame{
+    //Standardization of Frames
+    public static JFrame setFrame = new JFrame();
+
     public static void main(String[] args) {
-        new StartFrame();
+        new StartFrame(setFrame);
     }
 }
