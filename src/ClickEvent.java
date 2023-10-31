8d33ded4af0d2c6242a4d85e1d7ca4a6384c5220
SteffenLee <tnqjae@naver.com>
2023-10-31 15:32:45 +0900

    Add JFrame initialization class

--- src/ClickEvent.java
+++ src/ClickEvent.java
@@ -1,13 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+class Init { //Init JFrame class
+    public Init(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() { //Using Threads for Real-Time Processing
+                //JFrame Init
+                Main.setFrame.getContentPane().removeAll();
+                Main.setFrame.revalidate();
+                Main.setFrame.repaint();
+            }
+        });
+        thread.start();
+    }
 
-class StartClickEvent extends MouseAdapter{
+}
+class StartClickEvent extends MouseAdapter {
+
+    public StartClickEvent() {
+        //Dumy constructor
+    }
     public void mouseClicked(MouseEvent e) {
-        System.out.println("test");
+        new Init();
     }
+
 }
-class RuleClickEvent extends MouseAdapter{
-    public void mouseClicked(MouseEvent e){
-        System.out.println("Rule");
+
+class RuleClickEvent extends MouseAdapter {
+    public RuleClickEvent() {
+        //Dumy constructor
     }
+    public void mouseClicked(MouseEvent e) {
+        new Init();
+    };
 }
+
+
