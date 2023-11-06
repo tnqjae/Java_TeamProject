8058bdeb2faaaf4ce2045d1bcc2d5c624d907ca5
SteffenLee <tnqjae@naver.com>
2023-11-07 00:54:33 +0900

    Add MusicPlaying class

--- src/MusicPlay.java
+++ src/MusicPlay.java
@@ -1,12 +1,30 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
 public class MusicPlay{
-    public MusicPlay(){
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
+    private Clip clip;
 
-            }
-        });
-
+    public MusicPlay(String filePath) {
+        try {
+            File audioFile = new File(filePath); // 음악 파일의 경로로 변경해주세요.
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(audioFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public void musicPlay() {
+        if (clip != null) {
+            clip.start();
+        }
+    }
+
+    public void musicStop() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+            clip.close();//must be closed.
+        }
+    }
 }
