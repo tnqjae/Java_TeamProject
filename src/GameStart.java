ea3462623ae31ea93450650da22d4fe1d7f4727d
SteffenLee <tnqjae@naver.com>
2023-11-07 03:17:39 +0900

    Add function : Move Character

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
 
 public class GameStart extends ActiveFunction{
+    public static JLabel  boldmote;
     //Contains the overall content of the game
-    ImageIcon character;
+    public ImageIcon character;
     public GameStart(){
-        character = new ImageIcon("./img/test.png");
-
         ActiveFunction active = new ActiveFunction();
 
-        Main.setFrame.add(active.createLabel(character, 200,200));
+        character = new ImageIcon("./img/test.png");
+        boldmote = active.createLabel(character, 200,200);
+
+        addActiveListener();
+
+        Main.setFrame.add(boldmote);
+    }
+    public void addActiveListener(){
+        KeyboardEvent keyboardEvent = new KeyboardEvent(this); // GameStart 인스턴스 전달
+
+        boldmote.addKeyListener(keyboardEvent);
+        boldmote.setFocusable(true);
+        boldmote.requestFocusInWindow(); // boldmote에 포커스 요청
+
+        Main.setFrame.addKeyListener(keyboardEvent); // 프레임에도 KeyListener 추가
+        Main.setFrame.setFocusable(true);
     }
 }
