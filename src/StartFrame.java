7579d8e01f294a2a0eccee7c165fbe0cf87ccdb6
SteffenLee <tnqjae@naver.com>
2023-11-07 03:21:31 +0900

    Seperate ActiveFunction class and Edit code

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -12,13 +12,12 @@ public class StartFrame extends JFrame{
     protected StartFrame(){}
     public StartFrame(JFrame mainframe) {
         frameSetting(mainframe);
+        //프레임이 나타나는 위치 설정
+        mainframe.setLocation(300,300);
 
         MainDisplay runable = new MainDisplay();
         Thread th = new Thread(runable);
         th.start();
-        mainframe.setLocation(300,300);
-
-        ActiveFunction active = new ActiveFunction();
     }
 
     public void frameSetting(JFrame frame){
@@ -30,48 +29,6 @@ public class StartFrame extends JFrame{
     public void showFrame(JFrame frame){
         frame.setSize(1200, 750);
         frame.setVisible(true);
-    }
-}
-
-class ActiveFunction extends StartFrame{
-    protected ActiveFunction(){}
-    protected void loadComponent() {
-        bg = new ImageIcon("./img/StartBackground.jpeg");
-        titleImage = new ImageIcon("./img/title.png");
-        start = new ImageIcon("./img/Start.png");
-        rule = new ImageIcon("./img/Rule.png");
-        //Load music
-        music = new MusicPlay("./music/herrypotter.wav");
-    }
-    //Create Label
-    protected JLabel createLabel(ImageIcon icon, int x, int y){
-        JLabel la = new JLabel(icon);
-        la.setLocation(x,y);
-        la.setSize(icon.getIconWidth(),icon.getIconHeight());
-        return la;
-    }
-    //add Listener
-    protected void addEventListener(JLabel la, MouseAdapter listener){
-        la.addMouseListener(listener);
-    }
-    //Create and add component
-    protected void addComponent(){
-        //Set Title
-        Main.setFrame.add(createLabel(titleImage,350,80));
-        //Set Background
-        Main.setFrame.add(createLabel(bg, 0,0));
-        //Set Start Button
-        JLabel startLabel = createLabel(start, 60, 380);
-        addEventListener(startLabel, new StartClickEvent());
-        Main.setFrame.add(startLabel);
-        //Set Rule Button
-        JLabel ruleLabel = createLabel(rule, 60,500);
-        addEventListener(ruleLabel,new RuleClickEvent());
-        Main.setFrame.add(ruleLabel);
-    }
-    protected void UI(){
-        Main.setFrame.setSize(1200, 750);
-        Main.setFrame.setVisible(true);
     }
 }
 
