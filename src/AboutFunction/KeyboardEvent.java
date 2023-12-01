48a1bdb9e666bd3ddb4e8b1fbbe88774d3e2aa16
SteffenLee <tnqjae@naver.com>
2023-12-01 13:26:50 +0900

    Edit code: Image change code

--- src/AboutFunction/KeyboardEvent.java
+++ src/AboutFunction/KeyboardEvent.java
@@ -1,52 +1,126 @@
 package AboutFunction;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import MainFrame.GameStart;
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
-// 모든 키 이벤트 정의
 public class KeyboardEvent extends KeyAdapter {
-    ActiveFunction active = new ActiveFunction();
+
+    private ActiveFunction active = new ActiveFunction();
+    private Timer timer;
+    private int imageIndex; // 이미지 인덱스
+
+    public KeyboardEvent() {
+        // 타이머 설정
+        timer = new Timer(100, new ActionListener() { // 이미지를 0.2초 간격으로 변경
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 타이머가 갱신될 때마다 이미지 변경 메서드 호출
+                handleContinuousKeyPress();
+            }
+        });
+
+        imageIndex = 0; // 초기 이미지 인덱스 설정
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        ImageIcon refresh;
-        int keyCode = e.getKeyCode(); // 눌린 키의 코드를 가져옵니다.
+        int keyCode = e.getKeyCode();
 
+        // 왼쪽 또는 오른쪽 키가 눌렸을 때 타이머 시작
+        if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_RIGHT) {
+            timer.start();
+        }
+
+        // 이미지 변경 및 위치 조정
+        handleKeyPress(keyCode);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // 키가 떼졌을 때 타이머 중지
+        timer.stop();
+        // 이미지 인덱스 초기화
+        imageIndex = 0;
+    }
+
+    private void handleKeyPress(int keyCode) {
         int x = GameStart.boldmote.getX();
         int y = GameStart.boldmote.getY();
 
-        // 이동 전의 위치를 저장합니다.
+        // Save the position before moving.
         int oldX = x;
         int oldY = y;
 
-        // 키 입력에 따라 위치를 조정합니다.
-        if (keyCode == KeyEvent.VK_UP) {
-            y -= 10;
-            active.changeImg(GameStart.boldmote, "./img/character/herry_back.png");
-        } else if (keyCode == KeyEvent.VK_DOWN) {
-            y += 10;
-            active.changeImg(GameStart.boldmote, "./img/character/herry_front.png");
-        } else if (keyCode == KeyEvent.VK_LEFT) {
+        // Adjust the position based on the left or right arrow key.
+        if (keyCode == KeyEvent.VK_LEFT) {
             x -= 10;
-            active.changeImg(GameStart.boldmote, "./img/character/herry_left.png");
         } else if (keyCode == KeyEvent.VK_RIGHT) {
             x += 10;
-            active.changeImg(GameStart.boldmote, "./img/character/herry_right.png");
         }
 
-        // JFrame 영역을 벗어나면 위치를 수정합니다.
+        // Modify the position if it goes out of the JFrame area.
         if (isWithinBounds(x, y, GameStart.boldmote.getWidth(), GameStart.boldmote.getHeight())) {
             GameStart.boldmote.setLocation(x, y);
+
+            // 이미지 변경 메서드 호출
+            changeImages(keyCode);
         } else {
-            // 위치를 조정하지 않고 이동하지 않도록 이전 위치로 복원합니다.
+            // Restore to the previous position so that it doesn't move without adjusting its position.
             GameStart.boldmote.setLocation(oldX, oldY);
         }
     }
 
-    // 주어진 좌표가 JFrame 영역 내에 있는지 확인하는 메서드
+    private void handleContinuousKeyPress() {
+        // 계속해서 눌린 키에 대한 처리를 여기에 추가
+        // 예를 들어, 여러 이미지를 반복하여 변경할 수 있습니다.
+        // 여기서는 이미지 변경 로직을 타이머에서 처리하므로 불필요
+    }
+
+    private void changeImages(int keyCode) {
+        // 여러 이미지 경로를 순환하면서 변경
+        String[] images;
+
+        if (keyCode == KeyEvent.VK_LEFT) {
+            images = new String[] {
+                    "./img/character/herry_left.png",
+                    "./img/character/herry_left_walkLeftleg.png",
+                    "./img/character/herry_left_walkRightleg.png"
+            };
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+            images = new String[] {
+                    "./img/character/herry_right.png",
+                    "./img/character/herry_right_walkLeftleg.png",
+                    "./img/character/herry_right_walkRightleg.png"
+            };
+        } else if (keyCode == KeyEvent.VK_UP) {
+            images = new String[] {
+                    "./img/character/herry_right.png",
+                    "./img/character/herry_right_walkLeftleg.png",
+                    "./img/character/herry_right_walkRightleg.png"
+            };
+        }else if (keyCode == KeyEvent.VK_DOWN) {
+            images = new String[] {
+                    "./img/character/herry_front.png",
+                    "./img/character/herry_front_walkLeftleg.png",
+                    "./img/character/herry_front_walkRightleg.png"
+            };
+        } else {
+            return; // 다른 키는 처리하지 않음
+        }
+
+        active.changeImg(GameStart.boldmote, images[imageIndex]);
+
+        // 이미지 인덱스 갱신
+        imageIndex = (imageIndex + 1) % images.length;
+    }
+
+    // JFrame 영역 내에 위치하는지 확인하는 메서드
     private boolean isWithinBounds(int x, int y, int width, int height) {
         return x >= 0 && y >= 0 && x + width <= 1200 && y + height <= 750;
     }
